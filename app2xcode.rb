80d8a4ccc91410511945372a3121f1a59b7fdb93
Jim McKay <jimmckay@users.noreply.github.com>
2025-01-12 12:50:00 -0700

    Update app2xcode.rb

--- app2xcode.rb
+++ app2xcode.rb
@@ -1,8 +1,8 @@
 class app2xcode < Formula
   desc "Add a signed app to XCode's organizer"
   homepage "https://github.com/jimmckay/app2xcode/"
-  url "https://github.com/jimmckay/app2xcode/archive/v1.0.0.tar.gz"
-  sha256 ""
+  url "https://github.com/jimmckay/app2xcode/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "22a52001efe7f81c050eeae093e745797d66dc78d10702508e0519ebffcd04a9"
 
   def install
     bin.install "app2xcode"
